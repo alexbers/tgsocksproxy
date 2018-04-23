5d7544883d32b81ae3d8178ffc86c38dbb85f680
Alexander Bersenev <bay@hackerdom.ru>
2018-04-24 00:58:43 +0500

    more error handling

--- tgsocksproxy.py
+++ tgsocksproxy.py
@@ -208,7 +208,7 @@ async def handle_client(reader, writer):
                 else:
                     wr.write(data)
                     await wr.drain()
-        except (ConnectionResetError, AttributeError, BrokenPipeError):
+        except (ConnectionResetError, AttributeError, BrokenPipeError, OSError):
             wr.close()
 
     asyncio.ensure_future(connect_reader_to_writer(reader_tgt, writer))
